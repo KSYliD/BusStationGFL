f7e0da10f46132703edb3f6e48726ce0b3282829
Yaroslav Kondrasiuk <yvkondrasiuk@gmail.com>
2023-12-13 15:15:09 +0200

    Initial Commit

---
+++ src/main/java/com/example/busstationgfl/BusStationGflApplication.java
@@ -0,0 +1,13 @@
+package com.example.busstationgfl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusStationGflApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusStationGflApplication.class, args);
+    }
+
+}
